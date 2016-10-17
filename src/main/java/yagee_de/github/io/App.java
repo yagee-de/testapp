d6b5099c75dbb81c878ba81c51103927588e9eef
Thomas Scheffler <github@yagee.de>
2016-10-17 10:39:34 +0200

    Initial simple Java-Project

---
+++ src/main/java/yagee_de/github/io/App.java
@@ -0,0 +1,13 @@
+package yagee_de.github.io;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
